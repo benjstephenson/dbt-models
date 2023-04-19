49424ba9003eb63a414f3240129b2421e451f74d
Benji Stephenson <bjames.stephenson@gmail.com>
2023-04-20 11:28:26 +1200

    fix sql

--- models/example/location_enhanced.sql
+++ models/example/location_enhanced.sql
@@ -4,6 +4,6 @@ select l.city,
   l.iso_country_code as counry_code,
   c.iso_currency as currency_code,
   l.location
-  from LOCATION as l
-  join COUNTRY as c 
+  from HOLD_DB.LOCATION as l
+  join HOLD_DB.COUNTRY as c 
   on l.iso_country_code == c.iso_country
